0aeaaae3761d76b783d0643abbd72c389d9efa19
bit <mikemilligan.ua@gmail.com>
2020-12-24 19:12:53 +0200

    rollback

--- src/main/java/bit/happens/Inspection.java
+++ src/main/java/bit/happens/Inspection.java
@@ -2,11 +2,10 @@ package bit.happens;
 
 public interface Inspection {
 
-    void printArray(int[] a);
 
-    void minNumber(int[] a);
+    int minNumber(int[] a);
 
-    void maxNumber(int[] a);
+    int maxNumber(int[] a);
 
-    void agvNumber(int[] a);
+    double agvNumber(int[] a);
 }
